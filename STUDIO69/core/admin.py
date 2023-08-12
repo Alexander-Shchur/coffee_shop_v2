8127d268e126e6daf5d4cef81be921998b2b3f96
student <student@student.com>
2023-08-12 21:12:26 +0500

    Окончание пары 12.08.2023

--- STUDIO69/core/admin.py
+++ STUDIO69/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import *
+
 
 # Register your models here.
+@admin.register(Coffee)
+class CoffeeAdmin(admin.ModelAdmin):
+    list_display = [
+        i for i in Coffee.__dict__
+    ]
--- STUDIO69/core/models.py
+++ STUDIO69/core/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
 class AbstractProduct(models.Model):
-
     title = models.CharField(
         max_length=64,
         verbose_name='Название'
@@ -16,4 +16,57 @@ class AbstractProduct(models.Model):
         'Prices',
         on_delete=models.CASCADE,
         verbose_name='Цена'
+    )
+
+
+class Coffee(AbstractProduct):
+    ROASTING_CHOICES = [
+        ('Средняя', 'Средняя'),
+        ('Темная', 'Темная')
+    ]
+    MILL_CHOICES = [
+        ('Зерна', 'Зерна'),
+        ('Мелкий', 'Мелкий')
+    ]
+
+    country = models.ForeignKey(
+        'Country',
+        on_delete=models.CASCADE,
+        verbose_name='Страна'
+    )
+
+    roasting = models.CharField(
+        max_length=12,
+        choices=ROASTING_CHOICES,
+        default='Средняя',
+        verbose_name='Обжарка'
+    )
+
+    mill = models.CharField(
+        max_length=12,
+        choices=MILL_CHOICES,
+        default='Зерна',
+        verbose_name='Помол'
+    )
+
+    weight = models.DecimalField(
+        max_digits=15,
+        decimal_places=2,
+        verbose_name='Вес'
+
+    )
+
+
+class Prices(models.Model):
+    price = models.DecimalField(
+        max_digits=15,
+        decimal_places=2,
+        verbose_name='Цена'
+    )
+
+
+class Country(models.Model):
+    title = models.CharField(
+        max_length=64,
+        verbose_name='Название'
     )
